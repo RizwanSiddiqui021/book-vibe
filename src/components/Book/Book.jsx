1da2e09a4cb0a4f695b7d4a92fdfa61e1738bda0
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2025-03-05 16:28:41 +0600

    made the bookdetails page

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,23 +1,40 @@
 import React from "react";
+import starIcon from "../../assets/star.png";
+import { Link } from "react-router-dom";
 
 const Book = ({ book }) => {
-  const { image, bookName, author, category } = book;
+  const { image, bookName, author, category, tags, rating, bookId } = book;
 
   return (
-    <div className="border border-gray-200 p-6 rounded-2xl">
-      <div className="bg-[#F3F3F3] rounded-2xl flex justify-center items-center py-8">
-        <img className="h-[166px] w-[120px]" src={image} alt={bookName} />
+    <Link to={`/books/${bookId}`}>
+      <div className="border border-gray-200 p-6 rounded-2xl">
+        <div className="bg-[#F3F3F3] rounded-2xl flex justify-center items-center py-8">
+          <img className="h-[166px] w-[120px]" src={image} alt={bookName} />
+        </div>
+        <div className="flex gap-3 my-6">
+          {tags.map((tag, index) => (
+            <button
+              key={index}
+              className="bg-[#23BE0A0D] py-2 px-4 rounded-4xl text-[#23BE0A]"
+            >
+              {tag}
+            </button>
+          ))}
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold mb-4">{bookName}</h1>
+          <p className="font-medium text-[#131313CC]">By: {author}</p>
+        </div>
+        <div className="border border-dashed border-gray-200 my-5"></div>
+        <div className="flex items-center justify-between">
+          <p className="font-medium text-[#131313CC]">{category}</p>
+          <div className="flex items-center gap-1">
+            <p>{rating}</p>
+            <img className="w-[18px]" src={starIcon} alt="Star Icon" />
+          </div>
+        </div>
       </div>
-      <div></div>
-      <div>
-        <h1 className="text-2xl font-bold">{bookName}</h1>
-        <p className="font-medium text-[#131313CC]">By: {author}</p>
-      </div>
-      <div className="border border-dashed border-gray-200 my-5"></div>
-      <div>
-        <p className="font-medium text-[#131313CC]">{category}</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
---
+++ src/components/BookDetail/BookDetail.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+
+const BookDetail = () => {
+  const { bookId } = useParams();
+  const id = parseInt(bookId);
+
+  const data = useLoaderData();
+
+  const book = data.find((book) => book.bookId === id);
+  const {
+    image,
+    author,
+    bookName,
+    category,
+    tags,
+    review,
+    totalPages,
+    publisher,
+    yearOfPublishing,
+    rating,
+  } = book;
+  return (
+    <div className="m-6 grid grid-cols-1 lg:grid-cols-2 gap-12">
+      <div className="bg-[#F3F3F3] rounded-2xl flex justify-center items-center">
+        <img className="w-[360px] h-[540px]" src={image} alt={bookName} />
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold">{bookName}</h1>
+        <p className="text-[20px] text-[#131313CC]">By: {author}</p>
+        <hr className="text-gray-200 my-4" />
+        <p className="text-[20px] text-[#131313CC]">{category}</p>
+        <hr className="text-gray-200 my-4" />
+        <p className="font-bold">
+          Review: <span className="text-[#131313B3] font-normal">{review}</span>
+        </p>
+        <div className="mt-7 flex items-center gap-2">
+          <p className="font-bold">Tags:</p>
+          {tags.map((tag, index) => (
+            <button
+              key={index}
+              className="bg-[#23BE0A0D] py-2 px-4 rounded-4xl text-[#23BE0A]"
+            >
+              {tag}
+            </button>
+          ))}
+        </div>
+        <hr className="text-gray-200 my-4" />
+        <div className="flex items-center gap-10">
+          <div className="flex flex-col gap-3">
+            <p className="text-[#131313B3]">Number of Pages:</p>
+            <p className="text-[#131313B3]">Publisher:</p>
+            <p className="text-[#131313B3]">Year of Publishing:</p>
+            <p className="text-[#131313B3]">Rating:</p>
+          </div>
+          <div className="flex flex-col gap-3">
+            <h1 className="font-semibold">{totalPages}</h1>
+            <h1 className="font-semibold">{publisher}</h1>
+            <h1 className="font-semibold">{yearOfPublishing}</h1>
+            <h1 className="font-semibold">{rating}</h1>
+          </div>
+        </div>
+        <div className="mt-8 flex items-center gap-4">
+          <button className="px-8 py-4 border border-gray-400 rounded-[8px] font-semibold">
+            Read
+          </button>
+          <button className="bg-[#23BE0A] px-8 py-4 border border-[#23BE0A] rounded-[8px] font-semibold text-white">
+            Wishlist
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookDetail;
